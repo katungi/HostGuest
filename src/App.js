74908618093a20c3dc4f77b51537c7def26f5fc1
katungi <dankatdennis@gmail.com>
2020-09-30 07:23:55 +0300

    App layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-<div className="app">
-  <h1>HostGuest 🚀 </h1>
-</div>
+    <div className="app">
+      <h1>HostGuest 🚀 </h1>
+
+     {/* Home */}
+           {/* Header */}
+           {/* Banner */}
+              {/* Search */}
+           {/* Cards */}
+           {/* Footer */}
+
+      {/* Search Page */}     
+    </div>
   );
 }
 
 export default App;
-
