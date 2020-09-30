78351b6532de28094b929361bf5829a811ec67f8
katungi <dankatdennis@gmail.com>
2020-09-30 16:48:10 +0300

    dependency update

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Header.css";
+import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
+import ExpandMoreRoundedIcon from "@material-ui/icons/ExpandMoreRounded";
+import { pink } from "@material-ui/core/colors";
+import LanguageRoundedIcon from '@material-ui/icons/LanguageRounded';
+import { Avatar } from '@material-ui/core';
 
 function Header() {
   return (
@@ -8,13 +13,18 @@ function Header() {
         className="header__icon"
         src="https://hostguest.co.ke/wp-content/uploads/2018/12/hostguest-logo-2-300x87.png"
         alt="HostGuest"
-        
-        
       />
+
       <div className="header__center">
-          <input type="text">
-            <SearchIcon/>
-            
+        <input type="text" />
+        <SearchRoundedIcon style={{ color: pink[300] }} />
+      </div>
+
+      <div className="header__right">
+        <p>Become a Host</p>
+        <LanguageRoundedIcon style={{ color: pink[300] }} />
+        <ExpandMoreRoundedIcon/>
+        <Avatar/>
       </div>
     </div>
   );
