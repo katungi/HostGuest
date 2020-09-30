6559a36773ac57c82bb253a7637297ec92a1e7a6
katungi <dankatdennis@gmail.com>
2020-09-30 08:03:57 +0300

    🎺 header: added logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Home from './Home'
+import Header from './Header'
 
 function App() {
   return (
     <div className="app">
+      <Header/>
      {/* Home */}
      <Home/> 
            {/* Header */}
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__icon"
+        src="https://hostguest.co.ke/wp-content/uploads/2018/12/hostguest-logo-2-300x87.png"
+        alt="HostGuest"
+        
+      />
+    </div>
+  );
+}
+
+export default Header;
