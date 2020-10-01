5bd8a4a7dd28f6d4558b229d66240464d011a57c
katungi <dankatdennis@gmail.com>
2020-10-01 11:06:08 +0300

    set up react router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./Home";
 import Header from "./Header";
 import Footer from "./Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Search from "./Search";
 import SearchPage from "./SearchPage";
 
 function App() {
