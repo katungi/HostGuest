4d1efb321c5229c47b5b03d24b4d484a1025fd1a
katungi <dankatdennis@gmail.com>
2020-10-01 11:45:49 +0300

    🗂 feat: added cards for the result page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./SearchPage.css";
 import { Button } from "@material-ui/core";
+import SearchResult from './SearchResult'
 
 function SearchPage() {
+     
   return (
     <div className="searchPage">
       <div className="searchPage__info">
@@ -14,6 +16,33 @@ function SearchPage() {
         <Button variant="outlined">Rooms and beds</Button>
         <Button variant="outlined">More filters</Button>
       </div>
+      <SearchResult
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+        location="Private room in center of London"
+        title="Stay at this spacious Edwardian House"
+        description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+        star={4.73}
+        price="£30 / night"
+        total="£117 total"
+      />
+         <SearchResult
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+        location="Private room in center of London"
+        title="Stay at this spacious Edwardian House"
+        description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+        star={4.73}
+        price="£30 / night"
+        total="£117 total"
+      />
+         <SearchResult
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+        location="Private room in center of London"
+        title="Stay at this spacious Edwardian House"
+        description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+        star={4.73}
+        price="£30 / night"
+        total="£117 total"
+      />
     </div>
   );
 }
