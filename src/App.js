3b6c3cf8a4e085eb32762ff5f347cf6080b662d4
katungi <dankatdennis@gmail.com>
2020-10-01 10:53:34 +0300

    added react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
 import "./App.css";
-import Home from './Home'
-import Header from './Header'
-import Footer from './Footer'
+import Home from "./Home";
+import Header from "./Header";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Search from "./Search";
+import SearchPage from "./SearchPage";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-     <Home/> 
-     <Footer/>
-           {/* Header */}
-           {/* Banner */}
-              {/* Search */}
-           {/* Cards */}
-           {/* Footer */}
-
-      {/* Search Page */}     
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
