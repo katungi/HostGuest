addea8c1f6156d646b8aaff9e384aa4ca75ee3a4
katungi <dankatdennis@gmail.com>
2020-10-01 11:21:43 +0300

    fix: refactored footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <p>© All Rights Reserved</p>
-            <p>Made With 💟 by the HostGuest Team</p>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <p>Made With 💟 by the HostGuest Team 2020 ©</p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+    </div>
+  );
 }
 
- 
-
-export default Footer
+export default Footer;
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./SearchPage.css";
+import { Button } from "@material-ui/core";
 
 function SearchPage() {
   return (
@@ -7,6 +8,11 @@ function SearchPage() {
       <div className="searchPage__info">
         <p>62 stays · 26 august to 30 august · 2 guest</p>
         <h1>Stays nearby</h1>
+        <Button variant="outlined">Cancellation Flexibility</Button>
+        <Button variant="outlined">Type of place</Button>
+        <Button variant="outlined">Price</Button>
+        <Button variant="outlined">Rooms and beds</Button>
+        <Button variant="outlined">More filters</Button>
       </div>
     </div>
   );
