a96c0c7f23dfde15b93bd71a7410e989eeab5a86
katungi <dankatdennis@gmail.com>
2020-10-01 14:09:19 +0300

    changed app title

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <p>Made With Love by the HostGuest Team 2020 ©</p>
+      <p>Made With Love by the HostGuest Team 2020</p>
       <p>Privacy · Terms · Sitemap · Company Details</p>
     </div>
   );
